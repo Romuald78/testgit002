0542aa8e2f627bfeebe67b8987a048a53aa198ea
Romuald GRIGNON <romuald.grignon@gmail.com>
2022-04-18 15:54:54 +0200

    Modif dev#1

--- main.c
+++ main.c
@@ -4,5 +4,8 @@
 int main(){
     printf("Hello world !\n");
     printf("PI = %f\n", PI);
+    
+    printf("Modif DEV#1 avec un saut de ligne avant et un autre printf après\n");
+    printf("2ème printf du dev #1\n");
     return 0;
 }
